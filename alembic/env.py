da03947bf0f9e04e52470d6adc1b7edfd8969808
Cristian Moraes <cristian.hennequin1@gmail.com>
2024-08-04 18:52:52 -0300

    Atualizando o CRUD para o banco

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,15 @@
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from fast_api.models import table_registry
 from fast_api.settings import Settings
 
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
-
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -24,7 +25,7 @@ target_metadata = table_registry.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# ... etc
 
 
 def run_migrations_offline() -> None:
@@ -39,12 +40,13 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option('sqlalchemy.url')
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        dialect_opts={'paramstyle': 'named'},
+        compare_type=True,
     )
 
     with context.begin_transaction():
@@ -60,7 +62,7 @@ def run_migrations_online() -> None:
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+        prefix='sqlalchemy.',
         poolclass=pool.NullPool,
     )
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_api.schemas import UserPublic
+
 
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     # cliente = TestClient(app)  # ARRANGE (ORGANIZAÇÃO)
@@ -33,32 +35,37 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {'id': 1, 'username': 'TestUserName', 'email': 'test@test.com'}
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
             'id': 1,
-            'username': 'TestUserNamed2',
-            'email': 'test@test.com',
+            'username': 'Teste',
+            'email': 'teste@teste.com',
             'password': '123',
         },
     )
 
     assert response.json() == {
         'id': 1,
-        'username': 'TestUserNamed2',
-        'email': 'test@test.com',
+        'username': 'Teste',
+        'email': 'teste@teste.com',
     }
 
 
-def test_deleted_user(client):
+def test_deleted_user(client, user):
     response = client.delete('/users/1')
 
-    assert response.json() == {'message': 'User deleted!'}
+    assert response.json() == {'message': 'User deleted'}
