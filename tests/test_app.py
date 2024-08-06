26ed8c9bedf0c6187dc8444beac99b40dd30e335
Cristian Moraes <cristian.hennequin1@gmail.com>
2024-08-05 21:37:23 -0300

    Protege os endpoints GET, PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -47,11 +47,12 @@ def test_read_users_with_user(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
-            'id': 1,
+            'id': user.id,
             'username': 'Teste',
             'email': 'teste@teste.com',
             'password': '123',
@@ -59,13 +60,28 @@ def test_update_user(client, user):
     )
 
     assert response.json() == {
-        'id': 1,
+        'id': user.id,
         'username': 'Teste',
         'email': 'teste@teste.com',
     }
 
 
-def test_deleted_user(client, user):
-    response = client.delete('/users/1')
+def test_deleted_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token/',
+        data={'username': user.username, 'password': user.clean_password},
+    )
+
+    token = response.json()
+    assert response.status_code == HTTPStatus.OK
+    assert token['token_type'] == 'Bearer'
+    assert 'access_token' in token
